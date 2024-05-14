d87d150111a7837c3b8924a2eeeb82d35e7ad188
NaomiRussi <127900418+NaomiRussi@users.noreply.github.com>
2024-05-13 21:54:37 -0300

    Correções ap.py

--- ap.py
+++ ap.py
@@ -1,55 +1,46 @@
-from flask import Flask, render_template, request, json
-from flask_mysqldb import MySQL
+from flask import Flask, json, render_template, request
+from flaskext.mysql import MySQL
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "impacta2024"
-app.config['MYSQL_DB'] = "teste"
-app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
+
 mysql = MySQL(app)
 
 @app.route('/')
-def main ():
+def teste1():
     return render_template('aulamvc.html')
 
-
-# @app.route('/showSignUp')
-# def showSignUp():
-#     return render_template('signup.html')
-
-
-
 @app.route('/gravar', methods=['POST'])
-def gravar ():
+def teste2():
     try:
-        nomerec= request.form['nome']
+        nomerec = request.form['nome']
         emailrec = request.form['email']
         senharec = request.form['senha']
+        print("teste")
         print(nomerec)
         print(emailrec)
         print(senharec)
-
+        
         if nomerec and emailrec and senharec:
-
-            conn = mysql.connection
+            
+            conn =  mysql.connect()
             cursor = conn.cursor()
             _hashed_password = senharec
-            cursor.execute('insert into tbl_user (user_name, user_username, user_password) VALUES (%s, %s, %s)', ( nomerec,emailrec,_hashed_password))
+            cursor.execute('insert into tbl_user (user_name, user_username, user_password) VALUES (%s, %s, %s)', (nomerec, emailrec, _hashed_password))
             conn.commit()
-
-
+            
             return render_template('aulamvc.html')
         else:
             return json.dumps({'html':'<span>Enter the required fields</span>'})
-
     except Exception as e:
         return json.dumps({'error':str(e)})
-    finally:
-        # cursor.close()
-        # conn.close()
-        print('ops')
+    # else:
+        
 
-if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
